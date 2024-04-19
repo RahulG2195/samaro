83524fd967b59518cedff30309f37e110dccf254
Siddhesh-Dhamale <webdeveloper.crezvatic@gmail.com>
2024-04-19 17:47:38 +0530

    subanner

--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -1,4 +1,5 @@
 import SubBanner from '../../Components/SubBanner/SubBanner';
+import SubBanner from '@/Components/SubBanner/SubBanner';
 import '../contact.css';
 
 export default function page() {
