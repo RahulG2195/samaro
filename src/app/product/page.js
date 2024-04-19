7fec6194314c490b504b67eec5322236f6941f43
Siddhesh-Dhamale <webdeveloper.crezvatic@gmail.com>
2024-04-19 17:33:15 +0530

    trying to live 1

--- src/app/product/page.js
+++ src/app/product/page.js
@@ -1,11 +1,12 @@
 "use client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Topbar from "@/components/Home/Topbar/Topbar";
+import Topbar from "../../components/Home/Topbar/Topbar";
 import { useEffect } from "react";
-import Header from "@/components/Home/Header/Header";
-import Footer from "@/components/Home/Footer/Footer";
-import Products from "@/components/Product/products";
-import SpcBanner from "@/components/SpcProducts/SpcBanner";
+import Header from "../../components/Home/Header/Header";
+import Footer from "../../components/Home/Footer/Footer";
+// import Products from "@/components/Product/products";
+import Products from '../../components/Product/Products'
+import SpcBanner from "../../components/SpcProducts/SpcBanner";
 
 
 export default function Home() {
