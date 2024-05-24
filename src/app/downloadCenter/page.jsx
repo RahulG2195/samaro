1b7b57e609cc5a39817c806933466a10f049b5b3
Dincz <dineshndr02@gmail.com>
2024-05-24 11:40:04 +0530

    removed caretips and done newlesletter and download center

--- src/app/downloadCenter/page.jsx
+++ src/app/downloadCenter/page.jsx
@@ -13,7 +13,7 @@ import DownloadSwiper from '@/components/Downloadcenter/DownloadSwiper';
 const page = () => {
   const [activeCategory, setActiveCategory] = useState('All');
 
-  const cardData = [
+  const BrocardData = [
     {
       imgurl: '/assets/images/brochure/Brochure.png',
       title: 'Seven tips to help you get better flooring design & Concept',
@@ -26,48 +26,24 @@ const page = () => {
       date: 'November 11,2023 |',
       author: 'By Anand Kashyap'
     },
+  ]
+  const InstallationcardData = [
     {
       imgurl: '/assets/images/brochure/Brochure.png',
       title: 'Seven tips to help you get better flooring design & Concept',
       date: 'November 11,2023 |',
       author: 'By Anand Kashyap'
     },
-    // {
-    //   imgurl: '/assets/images/Newletter/samaro_spc_flooring_lookbook_front-1.png',
-    //   title: 'Seven tips to help you get better flooring design & Concept',
-    //   date: 'November 11,2023',
-    //   author: 'Anand Kashyap'
-    // },
-    // {
-    //   imgurl: '/assets/images/Newletter/samaro_spc_flooring_lookbook_front-1.png',
-    //   title: 'Seven tips to help you get better flooring design & Concept',
-    //   date: 'November 11,2023',
-    //   author: 'Anand Kashyap'
-    // },
-    // {
-    //   imgurl: '/assets/images/Newletter/samaro_spc_flooring_lookbook_front-1.png',
-    //   title: 'Seven tips to help you get better flooring design & Concept',
-    //   date: 'November 11,2023',
-    //   author: 'Anand Kashyap'
-    // },
-    // {
-    //   imgurl: '/assets/images/Newletter/samaro_spc_flooring_lookbook_front-1.png',
-    //   title: 'Seven tips to help you get better flooring design & Concept',
-    //   date: 'November 11,2023',
-    //   author: 'Anand Kashyap'
-    // },
-    // {
-    //   imgurl: '/assets/images/Newletter/samaro_spc_flooring_lookbook_front-1.png',
-    //   title: 'Seven tips to help you get better flooring design & Concept',
-    //   date: 'November 11,2023',
-    //   author: 'Anand Kashyap'
-    // },
-    // {
-    //   imgurl: '/assets/images/Newletter/samaro_spc_flooring_lookbook_front-1.png',
-    //   title: 'Seven tips to help you get better flooring design & Concept',
-    //   date: 'November 11,2023',
-    //   author: 'Anand Kashyap'
-    // },
+    
+  ]
+  const WarrentycardData = [
+    {
+      imgurl: '/assets/images/brochure/Brochure.png',
+      title: 'Seven tips to help you get better flooring design & Concept',
+      date: 'November 11,2023 |',
+      author: 'By Anand Kashyap'
+    },
+    
   ]
   return (
 
@@ -84,18 +60,18 @@ const page = () => {
           <button onClick={() => setActiveCategory('Installation Guide')} 
           className={`btn text-danger border-danger rounded-pill py-0 px-4 me-2 fw-semibold ${activeCategory === 'Installation Guide' && 'bg-danger text-white'}`}>Installation Guide</button>
 
-          <button onClick={() => setActiveCategory('Care Tips')}
-          className={`btn text-danger border-danger rounded-pill py-0 px-4 me-2 fw-semibold ${activeCategory === 'Care Tips' && 'bg-danger text-white'}`}>Care Tips</button>
+          {/* <button onClick={() => setActiveCategory('Care Tips')}
+          className={`btn text-danger border-danger rounded-pill py-0 px-4 me-2 fw-semibold ${activeCategory === 'Care Tips' && 'bg-danger text-white'}`}>Care Tips</button> */}
 
           <button onClick={() => setActiveCategory('Warranty')} 
           className={`btn text-danger border-danger rounded-pill py-0 px-4 me-2 fw-semibold ${activeCategory === 'Warranty' && 'bg-danger text-white'}`}>Warranty</button>
         </div>
       </div>
 
-      <DownloadSwiper title="Brochure" cardData={cardData} active={activeCategory === 'All' || activeCategory === 'Brochure'} />
-      <DownloadSwiper title="Installation Guide" cardData={cardData} active={activeCategory === 'All' || activeCategory === 'Installation Guide'} />
-      <DownloadSwiper title="Care Tips" cardData={cardData} active={activeCategory === 'All' || activeCategory === 'Care Tips'} />
-      <DownloadSwiper title="Warranty" cardData={cardData} active={activeCategory === 'All' || activeCategory === 'Warranty'} />
+      <DownloadSwiper title="Brochure" cardData={BrocardData} active={activeCategory === 'All' || activeCategory === 'Brochure'} />
+      <DownloadSwiper title="Installation Guide" cardData={InstallationcardData} active={activeCategory === 'All' || activeCategory === 'Installation Guide'} />
+      {/* <DownloadSwiper title="Care Tips" cardData={cardData} active={activeCategory === 'All' || activeCategory === 'Care Tips'} /> */}
+      <DownloadSwiper title="Warranty" cardData={WarrentycardData} active={activeCategory === 'All' || activeCategory === 'Warranty'} />
 
 
 
