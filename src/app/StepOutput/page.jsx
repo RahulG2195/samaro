09fc10721a09d2c57c35fd41d34b5df5b7e5cd78
Dincz <dineshndr02@gmail.com>
2024-05-27 15:02:31 +0530

    changes done

--- src/app/StepOutput/page.jsx
+++ src/app/StepOutput/page.jsx
@@ -16,7 +16,7 @@ const StepOutput = () => {
                 <h6>Laminate flooring</h6>
                 <p>Great looks at a great price: that’s what laminate has to offer. Enjoy an exceptionally natural-looking floor and give your room the warmth and look of real wood at a budget-friendly price.</p>
               </div>
-              <div className='prod_pointer'>
+              <div className='prod_pointer py-2'>
                 <ul>
                   <li><i className="fa-solid fa-check"></i> Budget Friendly</li>
                   <li><i className="fa-solid fa-check"></i> Scratch & Stain resistant</li>
