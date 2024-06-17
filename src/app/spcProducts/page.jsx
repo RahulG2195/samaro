f27df943a4d8382d05b66cb5452bdecbe1c6a30a
Siddhesh-Dhamale <webdeveloper.crezvatic@gmail.com>
2024-06-17 11:40:15 +0530

    resolving error in spc lvt

--- src/app/spcProducts/page.jsx
+++ src/app/spcProducts/page.jsx
@@ -1,37 +1,37 @@
-// "use client"
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import Footer from '@/components/Home/Footer/Footer'
-// import Header from '@/components/Home/Header/Header'
-// import Topbar from '@/components/Home/Topbar/Topbar'
-// import SpcBanner from '@/components/SpcProducts/SpcBanner'
-// import React, { useEffect } from 'react'
-// import SpcForm from "@/components/SpcProducts/SpcForm";
-// import Collection from "@/components/SpcProducts/Collection";
-// import Broucher from "@/components/SpcProducts/Broucher";
-// import SpcPage from "@/components/SpcProducts/SpcPage/SpcPage";
-// import CallToAction from "@/components/Home/CallToAction/CallToAction";
-// import InstallationSteps from "@/components/SpcProducts/InstallationSteps";
+"use client"
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from '@/components/Home/Footer/Footer'
+import Header from '@/components/Home/Header/Header'
+import Topbar from '@/components/Home/Topbar/Topbar'
+import SpcBanner from '@/components/SpcProducts/SpcBanner'
+import React, { useEffect } from 'react'
+import SpcForm from "@/components/SpcProducts/SpcForm";
+import Collection from "@/components/SpcProducts/Collection";
+import Broucher from "@/components/SpcProducts/Broucher";
+import SpcPage from "@/components/SpcProducts/SpcPage/SpcPage";
+import CallToAction from "@/components/Home/CallToAction/CallToAction";
+import InstallationSteps from "@/components/SpcProducts/InstallationSteps";
 
-// const page = () => {
-//     useEffect(() => {
-//         import("bootstrap/dist/js/bootstrap.bundle.min.js");
-//     }, []);
-//     return (
-//         <>
-//             {/* <Topbar></Topbar> */}
-//             {/* <Header></Header> */}
-//             <SpcBanner></SpcBanner>
-//             <SpcForm></SpcForm>
-//             <Collection></Collection>
-//             <Broucher></Broucher>
-//             <SpcPage></SpcPage>
-//             <InstallationSteps></InstallationSteps>
-//             <div className="mb-5 pb-5">
-//             <CallToAction></CallToAction></div>
-//             {/* <Footer></Footer> */}
-//         </>
+const page = () => {
+    useEffect(() => {
+        import("bootstrap/dist/js/bootstrap.bundle.min.js");
+    }, []);
+    return (
+        <>
+            {/* <Topbar></Topbar> */}
+            {/* <Header></Header> */}
+            {/* <SpcBanner></SpcBanner>
+            <SpcForm></SpcForm>
+            <Collection></Collection>
+            <Broucher></Broucher>
+            <SpcPage></SpcPage>
+            <InstallationSteps></InstallationSteps>
+            <div className="mb-5 pb-5">
+            <CallToAction></CallToAction></div> */}
+            {/* <Footer></Footer> */}
+        </>
 
-//     )
-// }
+    )
+}
 
-// export default page
+export default page
--- src/components/SpcProducts/SpcPage/SpcPage.jsx
+++ src/components/SpcProducts/SpcPage/SpcPage.jsx
@@ -1,64 +1,64 @@
-import Filters from '@/components/Product/Filters'
-import ProductCard from '@/components/Product/ProductCard'
-import Products from '@/components/Product/Products'
-// import "@/components/Home/Social/Social.css";
-import Inspiration from '@/components/Home/Social/Inspiration';
+// import Filters from '@/components/Product/Filters'
+// import ProductCard from '@/components/Product/ProductCard'
+// import Products from '@/components/Product/Products'
+// // import "@/components/Home/Social/Social.css";
+// import Inspiration from '@/components/Home/Social/Inspiration';
 
-const SpcPage = () => {
-    const productsArr = [
-        { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
-        { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
-        { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
-        { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
-        { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
-        { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
-        { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
-        { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
-        { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
-        { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
-        { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
-        { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
-        { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
-        // Adjust paths as necessary for additional products
-    ];
-    return (
-        <>
-            <div className="pe-5 ps-3 position-relative prdctContainer">
-                <div className="row">
-                    {/* Filters */}
-                    <div className="col-md-2 ">
-                        <Filters
-                            hide='hidethis'
-                            inSPC='hamInSPC'
-                        />
-                    </div>
-                    <div className="col-md-10">
-                        <div className="row align-items-center">
+// const SpcPage = () => {
+//     const productsArr = [
+//         { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
+//         { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
+//         { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
+//         { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
+//         { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
+//         { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
+//         { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
+//         { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
+//         { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
+//         { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
+//         { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
+//         { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
+//         { frontImage: "assets/images/products/Group 28396.png", onHoverImage: "assets/images/products/Group 28395.png", title: "Pure oak honey", description: "VINYL | CRIO | AVHBU40360" },
+//         // Adjust paths as necessary for additional products
+//     ];
+//     return (
+//         <>
+//             <div className="pe-5 ps-3 position-relative prdctContainer">
+//                 <div className="row">
+//                     {/* Filters */}
+//                     <div className="col-md-2 ">
+//                         <Filters
+//                             hide='hidethis'
+//                             inSPC='hamInSPC'
+//                         />
+//                     </div>
+//                     <div className="col-md-10">
+//                         <div className="row align-items-center">
 
-                            {/* Search Bar and Options */}
-                        </div>
-                        <p className='py-3 text-navy d-lg-none'>Choose Your <strong className='fw-medium'>product</strong></p>
-                        <div className="row row-cols-md-3 row-cols-sm-3 row-cols-xs-1 row-cols-lg-3 row-cols-xl-5">
-                            {productsArr.map((product, index) => (
-                                <div key={index} className="col-6 mb-4">
-                                    <ProductCard
-                                        frontImage={product.frontImage}
-                                        onHoverImage={product.onHoverImage}
-                                        title={product.title}
-                                        prod_code={product.prod_code}
-                                        variation={product.variation}
-                                        cat_name={product.cat_name}
-                                    />
-                                </div>
-                            ))}
-                        </div>
+//                             {/* Search Bar and Options */}
+//                         </div>
+//                         <p className='py-3 text-navy d-lg-none'>Choose Your <strong className='fw-medium'>product</strong></p>
+//                         <div className="row row-cols-md-3 row-cols-sm-3 row-cols-xs-1 row-cols-lg-3 row-cols-xl-5">
+//                             {productsArr.map((product, index) => (
+//                                 <div key={index} className="col-6 mb-4">
+//                                     <ProductCard
+//                                         frontImage={product.frontImage}
+//                                         onHoverImage={product.onHoverImage}
+//                                         title={product.title}
+//                                         prod_code={product.prod_code}
+//                                         variation={product.variation}
+//                                         cat_name={product.cat_name}
+//                                     />
+//                                 </div>
+//                             ))}
+//                         </div>
 
-                    </div>
-                </div>
-                <Inspiration />
-            </div>
-        </>
-    )
-}
+//                     </div>
+//                 </div>
+//                 <Inspiration />
+//             </div>
+//         </>
+//     )
+// }
 
-export default SpcPage
+// export default SpcPage
